c4aa691dc537054e5b79230d60f632b47fd5dc33
Morris, Kevin <Kevin.Morris@noblis.org>
2024-02-06 18:42:04 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <div>{{ demo }}</div>
+    <div>{{ demoUpper }}</div>
+    <div>{{ demoLower }}</div>
+    <div>{{ demoSnake }}</div>
+    <div>{{ demoKebab }}</div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import gladwell from "gladwell";
+
+const demo = "Hello, World!";
+
+const demoUpper = gladwell.upper(demo);
+const demoLower = gladwell.lower(demo);
+const demoSnake = gladwell.snake(demo);
+const demoKebab = gladwell.kebab(demo);
+</script>
+
+<style scoped></style>
